1ec3b73dd3af33de826dd56c4d770547909cbd05
maks969 <111703818+maks969@users.noreply.github.com>
2022-10-27 00:02:11 +0300

    prog & file .md

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿string[] newArr = {"sdtf","dfw","rr","h","erfvsd","jkssssswefseg"," "};
+var resultArr = newArr.Where(x=>x.Length < 4).ToArray();
+for (int i = 0; i < resultArr.Length; i++)
+      Console.Write(@"""{1},"" ", i, resultArr[i]);
